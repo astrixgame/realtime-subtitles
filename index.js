e8c927d6fa0330059c3fd7073e3c3d3ac3ea9bd0
AstrixGame <mail.astrixgame@gmail.com>
2024-07-16 20:44:56 +0200

    Using electron

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserView, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+    var win = new BrowserWindow({ width: 800, height: 1500, autoHideMenuBar: true, title: "Real-time subtitles", icon: "icon.ico" });
+
+    var view = new BrowserView();
+    var bounds = win.getContentBounds();
+
+    win.setBrowserView(view);
+    view.setBounds({ x: 0, y: 0, width: bounds.width, height: bounds.height });
+    win.on('resize', () => {
+        bounds = win.getContentBounds();
+        view.setBounds({ x: 0, y: 0, width: bounds.width, height: bounds.height })
+    });
+    view.webContents.loadFile('app/index.html')
+    view.setAutoResize({ width: true, height: true });
+});
